9e8847590ad2b75d497f9ee7955b4f8c0ffe2d8b
Lawdut <75577311+Lawdut@users.noreply.github.com>
2021-04-27 09:49:16 +0200

    ajout VueJs

---
+++ VueJs/mumandfit/src/components/navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <ul>
+            <li>Actualité</li>
+            <li>Prendre un rendez-vous</li>
+            <li>E-Boutiques</li>
+            <li>Connexion</li>
+        </ul> 
+    </nav>    
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+
+})
+</script>
+
+<style scoped>
+nav ul {
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
