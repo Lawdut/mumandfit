d41d8b2bdf8a52f5365dcee76312f3ace95ef980
Lawdut <75577311+Lawdut@users.noreply.github.com>
2021-05-05 00:45:11 +0200

    bdd

--- VueJs/mumandfit/src/components/Article.vue
+++ VueJs/mumandfit/src/components/Article.vue
@@ -1,12 +1,14 @@
 <template>
     <div class = "UnArticle">
         <div class = "vueArticle">
-            <span v-html="$store.state.unArticle.contenu" width="300" height="200">{{unArticle.contenu}}</span>
-            <button v-on:click="enableEditMode()">Editer</button>
+            <span v-html="$store.state.unArticle.contenu">{{unArticle.contenu}}</span>
             <div>
+                <button v-on:click="enableEditMode()">Editer</button>
                 <button v-on:click="execCmd('bold');">Gras</button>
             </div>
-            <iframe id="frame" name ="richTextField" ></iframe>
+            
+            <iframe id="frame" name ="richTextField" frameborder="0"></iframe>
+            
         </div>
     </div>
 </template>
@@ -14,8 +16,8 @@
 <script>
 import { mapGetters} from 'vuex'
 import jQuery from "jquery";
-const $ = jQuery;
-window.$ = $;
+    const $ = jQuery;
+    window.$ = $;
 
     export default {
         name : "Article",
@@ -43,15 +45,16 @@ window.$ = $;
                frame.html(text);
             },
             enableEditMode: function () {
-                let richTextField =  document.getElementById('frame').contentWindow;
-                console.log(richTextField);
+                let richTextField = document.getElementById('frame').contentWindow;
                 richTextField.document.designMode = "on";
+                //document.getElementById('myElement').style.display = 'none'; -> pour faire une fonction édition avec appararition des boutons (display block pour les boutons)
             },
             execCmd(command) {
                 console.log(command);
                 let richTextField =  document.getElementById('frame').contentWindow;
                 richTextField.document.execCommand(command, false, 'test');
-            }
+                
+            },
 
         },
         computed: {
@@ -64,7 +67,10 @@ window.$ = $;
 span {
     visibility: hidden;
 }
+
 iframe{
-    border: 0 none;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 }
 </style>
